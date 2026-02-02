489287bd5c2f9b79bb325170d41d41a6887f8b31
Claudio Ferreira <cldgncvs91@gmail.com>
2026-02-02 15:55:12 -0300

    Adiciona o número do pokemon antes do nome

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,246 +1,250 @@
-import { useEffect, useState } from 'react';
-import { supabase } from '@/lib/supabase'
-import styles from './list.module.css';
-import type { User } from '@supabase/supabase-js';
+import { useEffect, useState } from "react";
+import { supabase } from "@/lib/supabase";
+import styles from "./list.module.css";
+import type { User } from "@supabase/supabase-js";
 
 type Pokemon = {
-  id: number
-  name: string
-  image: string
-}
+  id: number;
+  name: string;
+  image: string;
+};
 
 type ListProps = {
-  ownedIds: number[]
-  setOwnedIds: React.Dispatch<React.SetStateAction<number[]>>
-  user: User | null     // <-- adiciona aqui
-}
+  ownedIds: number[];
+  setOwnedIds: React.Dispatch<React.SetStateAction<number[]>>;
+  user: User | null; // <-- adiciona aqui
+};
 
-function List({ownedIds, setOwnedIds, user}: ListProps) {
-const [loading, setLoading] = useState(true)
-const [pokemons, setPokemons] = useState<Pokemon[]>([])
-const [onlyOwned, setOnlyOwned] = useState(false)
-const [onlyNotOwned, setOnlyNotOwned] = useState(false)
-const [search, setSearch] = useState('')
-const [debouncedSearch, setDebouncedSearch] = useState('')
-const isSearching = search !== debouncedSearch
+function List({ ownedIds, setOwnedIds, user }: ListProps) {
+  const [loading, setLoading] = useState(true);
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  const [onlyOwned, setOnlyOwned] = useState(false);
+  const [onlyNotOwned, setOnlyNotOwned] = useState(false);
+  const [search, setSearch] = useState("");
+  const [debouncedSearch, setDebouncedSearch] = useState("");
+  const isSearching = search !== debouncedSearch;
 
-const filteredPokemons = pokemons.filter((p) =>
-  p.name.toLowerCase().includes(debouncedSearch.toLowerCase())
-)
+  const filteredPokemons = pokemons.filter((p) =>
+    p.name.toLowerCase().includes(debouncedSearch.toLowerCase()),
+  );
 
-const visiblePokemons = filteredPokemons.filter(p => {
-  if (onlyOwned) {
-    return ownedIds.includes(p.id)
-  } else if (onlyNotOwned) {
-    return !ownedIds.includes(p.id)
-  } else return true
-})
+  const visiblePokemons = filteredPokemons.filter((p) => {
+    if (onlyOwned) {
+      return ownedIds.includes(p.id);
+    } else if (onlyNotOwned) {
+      return !ownedIds.includes(p.id);
+    } else return true;
+  });
 
-async function logout() {
-  const { error } = await supabase.auth.signOut()
+  async function logout() {
+    const { error } = await supabase.auth.signOut();
 
-  if (error) {
-    console.error(error)
-    return
-  }
-
-  setOwnedIds([])
-}
-
-async function getUserCards() {
-  const { data: userData } = await supabase.auth.getUser()
-  const user = userData.user
-
-  if (!user) return
-
-  const { data, error } = await supabase
-    .from('user_cards')
-    .select('pokemon_id')
-    .eq('user_id', user.id)
-
-  if (error) {
-    console.error(error)
-    return
-  }
-
-  setOwnedIds(data.map(item => item.pokemon_id))
-}
-
-async function toggleCard(pokemonId: number) {
-  const { data } = await supabase.auth.getUser()
-  const user = data.user
-  if (!user) return
-
-  const jaTenho = ownedIds.includes(pokemonId)
-
-  if (jaTenho) {
-    // remover
-    const { error } = await supabase
-      .from('user_cards')
-      .delete()
-      .eq('user_id', user.id)
-      .eq('pokemon_id', pokemonId)
-
-    if (!error) {
-      setOwnedIds(prev => prev.filter(id => id !== pokemonId))
+    if (error) {
+      console.error(error);
+      return;
     }
-  } else {
-    // inserir
-    const { error } = await supabase
-      .from('user_cards')
-      .insert({
+
+    setOwnedIds([]);
+  }
+
+  async function getUserCards() {
+    const { data: userData } = await supabase.auth.getUser();
+    const user = userData.user;
+
+    if (!user) return;
+
+    const { data, error } = await supabase
+      .from("user_cards")
+      .select("pokemon_id")
+      .eq("user_id", user.id);
+
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    setOwnedIds(data.map((item) => item.pokemon_id));
+  }
+
+  async function toggleCard(pokemonId: number) {
+    const { data } = await supabase.auth.getUser();
+    const user = data.user;
+    if (!user) return;
+
+    const jaTenho = ownedIds.includes(pokemonId);
+
+    if (jaTenho) {
+      // remover
+      const { error } = await supabase
+        .from("user_cards")
+        .delete()
+        .eq("user_id", user.id)
+        .eq("pokemon_id", pokemonId);
+
+      if (!error) {
+        setOwnedIds((prev) => prev.filter((id) => id !== pokemonId));
+      }
+    } else {
+      // inserir
+      const { error } = await supabase.from("user_cards").insert({
         user_id: user.id,
-        pokemon_id: pokemonId
-      })
+        pokemon_id: pokemonId,
+      });
 
-    if (!error) {
-      setOwnedIds(prev => [...prev, pokemonId])
+      if (!error) {
+        setOwnedIds((prev) => [...prev, pokemonId]);
+      }
     }
   }
-}
 
-async function getPokemons() {
-  const res = await fetch(
-    'https://pokeapi.co/api/v2/pokemon?limit=1025'
-  )
-  const data = await res.json()
+  async function getPokemons() {
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=1025");
+    const data = await res.json();
 
-  const pokemons = data.results.map((p: {url: string, name: string}) => {
-    const id = Number(p.url.split('/').at(-2))
-    return {
-      id,
-      name: p.name,
-      image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
-    }
-  })
+    const pokemons = data.results.map((p: { url: string; name: string }) => {
+      const id = Number(p.url.split("/").at(-2));
+      return {
+        id,
+        name: p.name,
+        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
+      };
+    });
 
-  setPokemons(pokemons)
-}
-
-function getLocalization(pokemonId: number) {
-  const index = pokemonId - 1
-
-  const folder = Math.floor(index / 540) + 1
-  const indexInFolder = index % 540
-
-  const page = Math.floor(indexInFolder / 9) + 1
-  const position = (indexInFolder % 9) + 1
-
-  return { folder, page, position }
-}
-
-useEffect(() => {
-  async function init() {
-    setLoading(true)
-    await getPokemons()
-    await getUserCards()
-    setLoading(false)
+    setPokemons(pokemons);
   }
 
-  init()
-}, [])
+  function getLocalization(pokemonId: number) {
+    const index = pokemonId - 1;
 
-useEffect(() => {
-  const timeout = setTimeout(() => {
-    setDebouncedSearch(search)
-  }, 300) // 300ms é um bom padrão
+    const folder = Math.floor(index / 540) + 1;
+    const indexInFolder = index % 540;
 
-  return () => clearTimeout(timeout)
-}, [search])
+    const page = Math.floor(indexInFolder / 9) + 1;
+    const position = (indexInFolder % 9) + 1;
+
+    return { folder, page, position };
+  }
+
+  useEffect(() => {
+    async function init() {
+      setLoading(true);
+      await getPokemons();
+      await getUserCards();
+      setLoading(false);
+    }
+
+    init();
+  }, []);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebouncedSearch(search);
+    }, 300); // 300ms é um bom padrão
+
+    return () => clearTimeout(timeout);
+  }, [search]);
 
   return (
     <div className={styles.pageContainer}>
       <div className={styles.header}>
-      <h4>Seja bem-vindo, {user?.user_metadata.display_name}</h4>
-<p onClick={logout}>Sair</p>
+        <h4>Seja bem-vindo, {user?.user_metadata.display_name}</h4>
+        <p onClick={logout}>Sair</p>
       </div>
 
       <div className={styles.content}>
-
         <div className={styles.search}>
-        <input
-  type="text"
-  placeholder="Buscar Pokémon"
-  value={search}
-  onChange={(e) => setSearch(e.target.value.toLowerCase())}
-/>
+          <input
+            type="text"
+            placeholder="Buscar Pokémon"
+            value={search}
+            onChange={(e) => setSearch(e.target.value.toLowerCase())}
+          />
+        </div>
+
+        <div className={styles.filter}>
+          <label className={styles.owned}>
+            Mostrar só os que eu tenho
+            <input
+              id="onlyOwned"
+              type="checkbox"
+              checked={onlyOwned}
+              onChange={() => {
+                setOnlyOwned((prev) => !prev);
+                setOnlyNotOwned(false);
+              }}
+            />
+            <span className={styles.checkmark} />
+          </label>
+
+          <label className={styles.owned}>
+            Mostrar só os que eu NÃO tenho
+            <input
+              id="onlyNotOwned"
+              type="checkbox"
+              checked={onlyNotOwned}
+              onChange={() => {
+                setOnlyNotOwned((prev) => !prev);
+                setOnlyOwned(false);
+              }}
+            />
+            <span className={styles.checkmark} />
+          </label>
+        </div>
+
+        {isSearching && (
+          <div className={styles.searching}>
+            <p>Buscando...</p>
+          </div>
+        )}
+
+        {loading && (
+          <div className={styles.searching}>
+            <p>Buscando...</p>
+          </div>
+        )}
+
+        {!loading && !isSearching && !visiblePokemons.length && (
+          <div className={styles.searching}>
+            <p>Não há nenhum resultado para exibir.</p>
+          </div>
+        )}
+
+        <div className={styles.pokemonContainer}>
+          {!isSearching &&
+            !loading &&
+            visiblePokemons.map((pokemon) => {
+              const tenho = ownedIds.includes(pokemon.id);
+              const { folder, page, position } = getLocalization(pokemon.id);
+
+              return (
+                <div
+                  className={styles.pokemonBox}
+                  key={pokemon.id}
+                  onClick={() => toggleCard(pokemon.id)}
+                  style={{
+                    cursor: "pointer",
+                    opacity: tenho ? 1 : 0.35,
+                    filter: tenho ? "none" : "grayscale(100%)",
+                    transition: "0.2s",
+                    padding: 8,
+                  }}
+                >
+                  <img src={pokemon.image} alt={pokemon.name} />
+
+                  <p>
+                    <span>#{pokemon.id} - </span> {pokemon.name}
+                  </p>
+
+                  <small style={{ fontSize: 12, opacity: 0.8 }}>
+                    P{folder} • F{page} • Pos {position}
+                  </small>
+                </div>
+              );
+            })}
+        </div>
       </div>
-
-      <div className={styles.filter}>
-
-<label className={styles.owned}>
-  Mostrar só os que eu tenho
-  <input
-  id="onlyOwned"
-    type="checkbox"
-    checked={onlyOwned}
-    onChange={() => {
-      setOnlyOwned(prev => !prev)
-      setOnlyNotOwned(false)
-    }}
-  />
- <span className={styles.checkmark}/>
-</label>
-
-<label className={styles.owned}>
-  Mostrar só os que eu NÃO tenho
-  <input
-  id="onlyNotOwned"
-    type="checkbox"
-    checked={onlyNotOwned}
-    onChange={() => {
-      setOnlyNotOwned(prev => !prev) 
-      setOnlyOwned(false)
-    }}
-  />
- <span className={styles.checkmark}/>
-</label>
-
-</div>
-
-
-{isSearching && <div className={styles.searching}>
-  <p >Buscando...</p></div>}
-
-  {loading && <div className={styles.searching}>
-  <p >Buscando...</p></div>}
-
-    {!loading && !isSearching && !visiblePokemons.length && <div className={styles.searching}>
-  <p >Não há nenhum resultado para exibir.</p></div>}
-  
-  <div className={styles.pokemonContainer}>
-    {!isSearching && !loading && visiblePokemons.map((pokemon) => {
-    const tenho = ownedIds.includes(pokemon.id)
-    const { folder, page, position } = getLocalization(pokemon.id)
-
-    return (
-      <div
-        className={styles.pokemonBox}
-        key={pokemon.id}
-        onClick={() => toggleCard(pokemon.id)}
-        style={{
-          cursor: 'pointer',
-          opacity: tenho ? 1 : 0.35,
-          filter: tenho ? 'none' : 'grayscale(100%)',
-          transition: '0.2s',
-          padding: 8
-        }}
-      >
-        <img src={pokemon.image} alt={pokemon.name} />
-        
-        <p>{pokemon.name}</p>
-
-        <small style={{ fontSize: 12, opacity: 0.8 }}>
-          P{folder} • F{page} • Pos {position}
-        </small>
-      </div>
-    )
-  })}
-  </div>
-</div>
-      </div>
-     
-  )
+    </div>
+  );
 }
 
-export default List
+export default List;
